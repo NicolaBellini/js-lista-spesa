8a1afcf820afab01d683ecb447f5d2de4a9a8d45
NicolaBellini <bellininicola4366@gmail.com>
2024-02-27 15:07:58 +0100

    [clear] js code exra

--- JS/script.js
+++ JS/script.js
@@ -7,28 +7,21 @@ const shopList = [
 ]
 
 const shopListExtracted = []
-
-
 const output = document.getElementById("output")
 let validExtraction = true
 
 while(validExtraction){
   
-  let i = 0;
- for(let i = 0; i < shopList.length; i++){
-    
-  shopItems = shopList[i];
-  shopListExtracted.push(shopItems);
- }
-
-  console.log(shopItems);
+  for(let i = 0; i < shopList.length; i++){
+    shopItems = shopList[i];
+    shopListExtracted.push(shopItems);
+  }
 
   if(shopList.length = shopListExtracted.length){
     validExtraction = false;
     output.innerHTML += ` list items= ${shopList}` 
   }
-
-  console.log(validExtraction, shopListExtracted);
+  
 }
 
 // (Math.floor(Math.random) * )
