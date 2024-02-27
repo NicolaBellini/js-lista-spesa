fb61c6afcc40cd50cd55338eeec65b5916d2ee34
NicolaBellini <bellininicola4366@gmail.com>
2024-02-27 15:05:27 +0100

    [add] while loop

--- JS/script.js
+++ JS/script.js
@@ -8,14 +8,27 @@ const shopList = [
 
 const shopListExtracted = []
 
-// while(shopListExtracted.length < shopList.length){
-  // estraggo un elemento random da shoppinglist e lo pusho dentro a shopListExtracted
-  for(let i = 0; i < shopList.length ; i++){
-   
-    shopItems = shopList[i]
-    shopListExtracted.push(shopItems)
-    console.log(shopList[i] , shopItems , shopListExtracted);
+
+const output = document.getElementById("output")
+let validExtraction = true
+
+while(validExtraction){
+  
+  let i = 0;
+ for(let i = 0; i < shopList.length; i++){
+    
+  shopItems = shopList[i];
+  shopListExtracted.push(shopItems);
+ }
+
+  console.log(shopItems);
+
+  if(shopList.length = shopListExtracted.length){
+    validExtraction = false;
+    output.innerHTML += ` list items= ${shopList}` 
   }
-// }
+
+  console.log(validExtraction, shopListExtracted);
+}
 
 // (Math.floor(Math.random) * )
